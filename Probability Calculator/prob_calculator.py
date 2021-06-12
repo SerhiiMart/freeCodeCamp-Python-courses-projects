332fbb6cf2fe5579a9f605a21215e1f42439fb39
Serhii <malgothsm@gmail.com>
2021-06-12 12:18:49 +0300

    Finished Hat class

--- Probability Calculator/prob_calculator.py
+++ Probability Calculator/prob_calculator.py
@@ -3,6 +3,25 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+  def __init__(object, **balls):
+        object.balls = dict(balls)
+        object.contents = []
+        for colour, number in object.balls.items():
+            for i in range(number):
+                object.contents.append(colour)
+
+  def draw(object, number_of_balls_to_draw):
+      rand_balls = []
+      temp_contents = object.contents
+      if number_of_balls_to_draw > len(object.contents):
+          return object.contents
+      for i in range(number_of_balls_to_draw):
+        rand_ball = random.choice(temp_contents)
+        rand_balls.append(rand_ball)
+        temp_contents.remove(rand_ball)
+      return rand_balls
+
+
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
