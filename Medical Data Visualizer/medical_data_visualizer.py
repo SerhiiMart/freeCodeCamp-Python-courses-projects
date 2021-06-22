82e0c715b97b2319bd0a1b577355bd2e6f801ca1
Serhii <malgothsm@gmail.com>
2021-06-22 16:41:24 +0300

    Small fixes

--- Medical Data Visualizer/medical_data_visualizer.py
+++ Medical Data Visualizer/medical_data_visualizer.py
@@ -18,7 +18,6 @@ def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(df, id_vars =["cardio"], value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active','overweight'])
 
-
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
     df_cat["total"] = 1
     df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index =False).count()
@@ -27,11 +26,9 @@ def draw_cat_plot():
     sns.set_theme(style="darkgrid")
     fig = sns.catplot(x = "variable", y = "total", data = df_cat, hue ="value", kind = "bar", col = "cardio").fig
 
-
     # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
-
 
 # Draw Heat Map
 def draw_heat_map():
@@ -49,14 +46,11 @@ def draw_heat_map():
     # Generate a mask for the upper triangle
     mask = np.triu(corr)
 
-
-
     # Set up the matplotlib figure
     fig, ax = plt.subplots(figsize=(12, 12)) 
 
     # Draw the heatmap with 'sns.heatmap()'
     sns.heatmap(corr, linewidths=1, annot = True, square = True, mask = mask, fmt =".1f",center =0.08,cbar_kws = {"shrink":0.5})
-
 
     # Do not modify the next two lines
     fig.savefig('heatmap.png')
