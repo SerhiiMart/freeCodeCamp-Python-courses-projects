3609c118d787a46d705e09b5855f2a216733b387
Serhii <malgothsm@gmail.com>
2021-06-23 16:26:50 +0300

    Working on visualizer

--- Page View Time Series Visualizer/time_series_visualizer.py
+++ Page View Time Series Visualizer/time_series_visualizer.py
@@ -5,18 +5,20 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv",parse_dates = ["date"], index_col = "date")
 
 # Clean data
-df = None
-
+df = df[
+  (df["value"] >= df["value"].quantile(0.025)) &
+  (df["value"] <= df["value"].quantile(0.975))]
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.plot(df.index, df["value"], 'r', linewidth=1)
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
